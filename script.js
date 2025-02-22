ca93614541794b684781cfb5ba3bccf133ba558b
your_username <kopparthichaithanyaraju@gmail.com>
2025-02-22 18:39:57 +0530

    updated the files

--- script.js
+++ script.js
@@ -1,51 +1,122 @@
-const button=document.getElementById("button");
-const Password=document.getElementById("password");
-const Username=document.getElementById("username");
+const result=document.getElementsByClassName("game-result");
+const showingoutput=document.getElementsByClassName("shownig-result");
 
-document.getElementById("button").addEventListener("click",  function()
-{
+ 
+const scores={
+    win:0,
+    loss:0,
+    tie:0,
+};
+function Paper(){
 
-  event.preventDefault();
-  
+    
+    let pickingMOVE =Math.random();
+    let computermove="";
 
-  const usernamevalue=Username.value;
-  
-  const userinput=document.getElementById("namefeild");
-  
-  if(usernamevalue===""){
-    userinput.textContent="Fill the Username feild!";
-    userinput.style.fontFamily="Microsoft JhengHei";
-    userinput.style.color="red";
-  }else{
-    userinput.textContent="";
-  }
-  
+    if(pickingMOVE<=0 && pickingMOVE<1/2){
+    computermove="scissors";
+    
+} else if(pickingMOVE<=1/2 && pickingMOVE<2/3){
+    computermove="rock";
+
+}   
+else if(pickingMOVE<=2/3 && pickingMOVE<1){
+    computermove="paper";
+} 
+
+if(computermove==="paper"){
+    console.log("Tie");
+    scores.tie +=1;
+}else if(computermove==="rock"){
+    console.log("You lose.");
+    scores.loss +=1;
+}else if(computermove==="scissors"){
+    console.log("You win.");
+    scores.win +=1;
+}
+document.getElementsByClassName("game-result")[0].
+innerHTML = `<p>computer move ${computermove}</p>
+              <p>wins: ${scores.win} 
+                loss: ${scores.loss} 
+                ties: ${scores.tie}</p>`;
 
 
-const passwordvalue=Password.value;
+}
+function Rock(){
 
-const passwordinput=document.getElementById("passfeild");
+    let pickingMOVE =Math.random();
+    let computermove="";
 
-if(passwordvalue===""){
-  passwordinput.textContent="Enter your password.";
-  passwordinput.style.color="red";
-  passwordinput.style.fontFamily="Microsoft JhangHei";
+    if(pickingMOVE<=0 && pickingMOVE<1/2){
+    computermove="paper";
+    
+} else if(pickingMOVE<=1/2 && pickingMOVE<2/3){
+    computermove="rock";
 
-}else{
-  passwordinput.textContent="";
+}   
+else if(pickingMOVE<=2/3 && pickingMOVE<1){
+    computermove="scissors";
+} 
+
+if(computermove==="paper"){
+    console.log("You loss");
+    scores.loss +=1;
+    
+}else if(computermove==="rock"){
+    console.log("Tie");
+    scores.tie +=1;
+}else if(computermove==="scissors"){
+    console.log("You win.");
+    scores.win +=1;
+}
+document.getElementsByClassName("game-result")[0].
+innerHTML = `  <p>computer move ${computermove}</p>
+               <p>wins: ${scores.win} 
+                loss: ${scores.loss} 
+                ties: ${scores.tie}</p>`;
+
 }
 
-buttonhandler(usernamevalue, passwordvalue);
+function Scissores(){
+
+    let pickingMOVE =Math.random();
+    let computermove="";
+    
+    if(pickingMOVE<=0 && pickingMOVE<1/2){
+    computermove="rock";
+    
+} else if(pickingMOVE<=1/2 && pickingMOVE<2/3){
+    computermove="paper";
+
+}   
+else if(pickingMOVE<=2/3 && pickingMOVE<1){
+    computermove="scissors";
+} 
 
 
-});
+if(computermove==="paper"){
+    console.log("You win.");
+    scores.win +=1;
+}else if(computermove==="rock"){
+    console.log("You lose.");
+    scores.loss +=1;
+}else if(computermove==="scissors"){
+    console.log("Tie.");
+    scores.tie +=1;
+}
 
-function buttonhandler(usernamevalue,passwordvalue){
-  if(usernamevalue !==""&& passwordvalue!==""){
-    alert("LOGIN COMPLETED!");
-    location.reload();
-  }else{
-    alert("LOGIN FAILED.");
-  }
-  
+document.getElementsByClassName("game-result")[0].
+innerHTML = `<p>computer move ${computermove}</p>
+                <p>wins: ${scores.win} 
+                loss: ${scores.loss} 
+                ties: ${scores.tie}</p>`;
+
+}
+function reset(){
+    scores.win=0;
+    scores.loss=0;
+    scores.tie=0;
+    document.getElementsByClassName("game-result")[0].innerHTML
+      =`<p>wins: ${scores.win} loss: ${scores.loss} tie: ${scores.tie}</P>`;
+      console.log("Rested");
 }
